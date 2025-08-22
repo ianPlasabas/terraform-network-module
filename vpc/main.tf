9de7ccb3e9ed705bb6fd50624272bed8e69c3472
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-08-22 16:02:58 +0800

    refactor code

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,17 +3,10 @@ resource "aws_vpc" "main" {
   instance_tenancy     = var.tenancy
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
-  tags = {
-    name = each.key
-  }
 }
 
 resource "aws_subnet" "private_main" {
   for_each   = var.private_subnet
   vpc_id     = aws_vpc.main.id
   cidr_block = each.value
-
-  tags = {
-    Name = "Main"
-  }
 }
