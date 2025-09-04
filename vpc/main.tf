72dbaa9f5d8994aab98417e08ed86896e593abbd
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-09-05 07:13:59 +0800

    Refactor

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,15 +2,6 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-locals {
-  local_web_subnet = toset(var.web)
-  all_azs = tolist(data.aws_availability_zones.available.names[*])
-  selected_azs    = slice(local.all_azs, 0, length(var.web))
-  local_subnets_by_az = zipmap(
-    tolist(var.web),
-    tolist(local.selected_azs)
-  )
-}
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   instance_tenancy     = var.tenancy
@@ -20,7 +11,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "web" {
-  for_each                = local.local_subnets_by_az
+  for_each                = var.web
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.key
   availability_zone       = each.value
