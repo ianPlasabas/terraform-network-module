a19dbc06c2d1851f7685389e492212c51288e42f
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-09-04 16:33:14 +0800

    Added validation for variable cidr_block

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -2,6 +2,10 @@ variable "cidr_block" {
   description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
+  validation {
+    condition = length(var.cidr_block) == 0
+    error_message = "Please provide a VPC CIDR"
+  }
 }
 
 variable "tenancy" {
