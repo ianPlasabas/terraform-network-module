79a13d7aaf5abcffbfb29cbb073a41a7ae808cd7
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-09-04 17:06:12 +0800

    Change local variable to respond based on the given user input

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,4 +1,6 @@
 locals {
+  # The number of subnets is now determined by the length of the
+  # CIDR list provided by the user.
   web_subnets = zipmap(
     data.aws_availability_zones.available.names[0:length(var.web)],
     var.web
