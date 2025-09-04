284289b70338cfeb2c9771d707b1295232b08022
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-09-04 21:55:28 +0800

    Refactor

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,11 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals {
+  # Create a map from the list, with numeric keys
+  web_subnets_map = { for idx, val in var.web : idx => val }
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   instance_tenancy     = var.tenancy
@@ -14,7 +19,7 @@ resource "aws_subnet" "web" {
   for_each                = toset(var.web)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = each.value.cidr_block
-  availability_zone       = data.aws_availability_zones.available.names[index(keys(var.web), each.key) % length(data.aws_availability_zones.available.names)]
+  availability_zone       = data.aws_availability_zones.available.names[each.key % length(data.aws_availability_zones.available.names)]
   map_public_ip_on_launch = true
   tags                    = var.web_tags
 }
