93026c577edd07f41af3a37091ad14388d69a794
Ian Paul Plasabas <iann.plasabas@gmail.com>
2025-09-04 19:32:53 +0800

    Refactor

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,10 +1,3 @@
-locals {
-  # This for expression creates a map where the key is the AZ name
-  # and the value is a map with both the AZ name and the CIDR block.
-  # The `zipmap` function is perfect for this.
-
-}
-
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   instance_tenancy     = var.tenancy
@@ -21,20 +14,3 @@ resource "aws_subnet" "web" {
   map_public_ip_on_launch = true
   tags                    = var.web_tags
 }
-
-resource "aws_subnet" "app" {
-  for_each          = var.app
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value
-  availability_zone = each.key
-  tags              = var.app_tags
-}
-
-resource "aws_subnet" "db" {
-  for_each          = var.db
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = each.value
-  availability_zone = each.key
-  tags              = var.db_tags
-}
-
